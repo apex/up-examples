9d6f240c70135c115c889c29898024be2a50aeaf
TJ Holowaychuk <tj@vision-media.ca>
2017-07-11 17:44:28 -0700

    add script to index

---
+++ index.sh
@@ -0,0 +1,6 @@
+
+for dir in $(find . -type d -depth 1 | grep -v .git); do
+  desc=$(grep -E '^# ' $dir/Readme.md -A 2 | tail -n 1)
+  name=$(echo $dir | sed 's|./||' | tr '-' ' ')
+  echo "- [$name]($dir) – $desc"
+done
