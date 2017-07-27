e330b174f84c13e3a2dc8b275c6f10c65f846211
TJ Holowaychuk <tj@vision-media.ca>
2017-07-26 18:16:15 -0700

    refactor test script

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 for plan in $(find . -type d -depth 1 | grep -v .git); do
   echo "==> $plan"
   for dir in $(find $plan -type d -depth 1); do
-    echo "  --> $dir"
-    up -C $dir config > /dev/null
+    printf "  --> $dir"
+    up -C $dir config 2>&1 > /dev/null
   done
 done
