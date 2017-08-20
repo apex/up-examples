a1a6f07862db5a883026ba8d11fd5dbfe57559ad
Shane Witbeck <shane.witbeck@gmail.com>
2017-08-20 10:37:38 -0700

    adds another Java runtime example using Maven for issue #32

---
+++ oss/java-maven-spark/src/main/java/com/github/apex/up/examples/spark/Application.java
@@ -0,0 +1,15 @@
+package com.github.apex.up.examples.spark;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.getenv;
+import static java.util.Optional.ofNullable;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class Application {
+  public static void main(String[] args) {
+    String port = ofNullable(getenv("PORT")).orElse("3000");
+    port(parseInt(port));
+    get("/", (request, response) -> "hello world");
+  }
+}
