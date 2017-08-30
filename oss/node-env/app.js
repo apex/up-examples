d579dd77a9fa0adaa267f1a061dbc60e5a54cc60
TJ Holowaychuk <tj@vision-media.ca>
2017-08-30 10:55:30 -0700

    refactor node-env example

--- oss/node-env/app.js
+++ oss/node-env/app.js
@@ -1,14 +1,15 @@
 const http = require('http')
 
 const { PORT = 3000 } = process.env
-const env = {}
 
-for (var key in process.env) {
-  if (key.indexOf('UP_') == 0) {
-    env[key] = process.env[key]
-  }
+const json = (res, v) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(v, null, 2))
 }
 
 http.createServer((req, res) => {
-  res.end(JSON.stringify(env, null, 2))
+  json(res, {
+    FOO: process.env.FOO,
+    BAR: process.env.BAR,
+  })
 }).listen(PORT)
