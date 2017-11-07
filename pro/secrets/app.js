0961339a80b8e8666575a94d842bf60a439a4545
TJ Holowaychuk <tj@vision-media.ca>
2017-11-06 20:17:54 -0800

    add some more vars to secrets example

--- pro/secrets/app.js
+++ pro/secrets/app.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const { PORT = 3000 } = process.env
 
+// Connect to a database here
+// const dbUser = process.env.DB_USER
+// const dbPass = process.env.DB_PASS
+
 http.createServer((req, res) => {
   const stage = process.env.UP_STAGE
   const msg = process.env.MSG
