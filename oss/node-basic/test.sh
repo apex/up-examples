aa866c6e6398eab78ee08cd14fff83362ef0252b
TJ Holowaychuk <tj@vision-media.ca>
2019-12-17 17:38:11 +0000

    fix typo in previous commit

--- oss/node-basic/test.sh
+++ oss/node-basic/test.sh
@@ -3,5 +3,5 @@ curl -s `up url`
 # equals: Hello World from AWS_Lambda_nodejs8.10
 
 up production
-curl -s `up url`
+curl -s `up url -s production`
 # equals: Hello World from AWS_Lambda_nodejs10.x
