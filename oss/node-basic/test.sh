78b6f7cd4fb5b3d03dc2297ecdf6bbd1861179e5
TJ Holowaychuk <tj@vision-media.ca>
2019-12-17 17:28:39 +0000

    add new node-basic overrides QA test

--- oss/node-basic/test.sh
+++ oss/node-basic/test.sh
@@ -1,4 +1,7 @@
 up
-
 curl -s `up url`
-# equals: Hello World from Node.js
+# equals: Hello World from AWS_Lambda_nodejs8.10
+
+up production
+curl -s `up url`
+# equals: Hello World from AWS_Lambda_nodejs10.x
