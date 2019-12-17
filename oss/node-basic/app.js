5cfd65de2f9c0a863be49ee5a040773c5043b5d6
TJ Holowaychuk <tj@vision-media.ca>
2019-12-17 17:05:17 +0000

    oss: add stage level lambda.runtime override example

--- oss/node-basic/app.js
+++ oss/node-basic/app.js
@@ -1,6 +1,6 @@
 const http = require('http')
-const { PORT = 3000 } = process.env
+const { PORT = 3000, AWS_EXECUTION_ENV } = process.env
 
 http.createServer((req, res) => {
-  res.end('Hello World from Node.js\n')
+  res.end(`Hello World from ${AWS_EXECUTION_ENV}`)
 }).listen(PORT)
